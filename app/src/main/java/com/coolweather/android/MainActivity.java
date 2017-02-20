05d0132df78b05a4c69fe24b5d3410d611388a5c
lsy <1507903919@qq.com>
2017-02-20 11:03:58 +0800

    加入创建数据库 和表的各项设置

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.coolweather;
+package com.coolweather.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
